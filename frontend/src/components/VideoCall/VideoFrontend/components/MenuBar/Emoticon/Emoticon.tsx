05fb5c14386176e1d29e12db57afaf40a51ee93a
carsongoffinet <goffinet.c@husky.neu.edu>
2022-03-27 19:41:09 -0400

    emoticon button displays on menu bar

---
+++ frontend/src/components/VideoCall/VideoFrontend/components/MenuBar/Emoticon/Emoticon.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Button from '@material-ui/core/Button';
+
+export default function Emoticon() {
+  return (
+    <Button
+    data-cy-more-button
+    >
+      Emoticon
+    </Button>
+  );
+}
