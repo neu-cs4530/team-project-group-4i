c45b7b02554612aa0e4c79b0660c08e8b2dcb85e
OFarmi <farmi.othman@gmail.com>
2022-03-28 18:34:48 -0400

    updated StatusMessage

---
+++ frontend/src/components/VideoCall/VideoFrontend/components/MenuBar/StatusMessage/StatusMessage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Button from '@material-ui/core/Button';
+
+export default function StatusMessage() {
+  return (
+    <Button
+    data-cy-more-Button
+    >
+      Status Message
+    </Button>
+  )
+}
