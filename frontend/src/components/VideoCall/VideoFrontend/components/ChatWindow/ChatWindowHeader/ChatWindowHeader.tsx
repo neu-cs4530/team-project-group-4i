5fced8a7ed1d655a8d93afc82d1e3caff36250b9
Dustin <yangdustin@outlook.com>
2022-04-10 14:55:25 -0400

    dropdown list of players is sorted alphanumerically

--- frontend/src/components/VideoCall/VideoFrontend/components/ChatWindow/ChatWindowHeader/ChatWindowHeader.tsx
+++ frontend/src/components/VideoCall/VideoFrontend/components/ChatWindow/ChatWindowHeader/ChatWindowHeader.tsx
@@ -5,6 +5,7 @@ import CloseIcon from '../../../icons/CloseIcon';
 import useChatContext from '../../../hooks/useChatContext/useChatContext';
 import usePlayersInTown from '../../../../../../hooks/usePlayersInTown';
 import useCoveyAppState from '../../../../../../hooks/useCoveyAppState';
+import Player from '../../../../../../classes/Player';
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -30,11 +31,23 @@ const useStyles = makeStyles(() =>
   })
 );
 
+/**
+ * Sorts the list of players alphanumerically such that numbers come before letters.
+ * Also for example 'name10' comes after 'name9'
+ * @param players List of players to sort
+ */
+ function alphanumericSort(players: Player[]): Player[] {
+  const collator = new Intl.Collator('en', { numeric: true, sensitivity: 'base' });
+  const sortedList = players.sort((player1, player2) => collator.compare(player1.userName, player2.userName));
+  return sortedList;
+}
+
 export default function ChatWindowHeader() {
   const classes = useStyles();
   const { setIsChatWindowOpen } = useChatContext();
   const { userName } = useCoveyAppState();
   const playersInTown = usePlayersInTown();
+  const sortedPlayers = alphanumericSort([...playersInTown]);
 
   return (
     <div className={classes.container}>
@@ -44,7 +57,7 @@ export default function ChatWindowHeader() {
           Everyone
         </option>
         {
-          playersInTown.filter(player => player.userName !== userName)
+          sortedPlayers.filter(player => player.userName !== userName)
             .map(player => 
             <option value={player.userName}>
               {player.userName}
